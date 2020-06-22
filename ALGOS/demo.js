8f7239e0a288b7f76115999c7257eac2ab12287b
Edward <eddieim@gmail.com>
2020-06-22 09:31:43 -0600

    rearranged weeks

---
+++ ALGOS/demo.js
@@ -0,0 +1,14 @@
+// MONDAY
+// GO OVER RIOT
+// Given an array, reverse the numbers within the array.
+function arrayReverse(arr){
+
+}
+// [1,2,3,4,5,6,7] => [7,6,5,4,3,2,1]
+// MONDAY ALTERNATE
+function moveZero(arr){
+
+}
+// Given an array, move all the zeroes to back of array
+// You may return a new array or for BONUS do it in-place with the help of the splice() method
+// [1,0,2,0,0,3,4,5] => [1,2,3,4,5,0,0,0]
